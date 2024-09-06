d6fe6b416fe802bad1087a928f0d14cad23ac8bb
suchitagithub <85994284+suchitagithub@users.noreply.github.com>
2024-09-06 18:25:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"
+}
+ 
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+ 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "user_data" {
+  description = "User data to configure the instance"
+  type        = string
+}
+ 
+variable "min_size" {
+  description = "Minimum size of the Auto Scaling Group"
+  type        = number
+  default     = 1
+}
+ 
+variable "max_size" {
+  description = "Maximum size of the Auto Scaling Group"
+  type        = number
+  default     = 3
+}
+ 
+variable "desired_capacity" {
+  description = "Desired capacity of the Auto Scaling Group"
+  type        = number
+  default     = 1
+}
+ 
+variable "target_group_arns" {
+  description = "List of Target Group ARNs for the Auto Scaling Group"
+  type        = list(string)
+}
+
+has context menu
